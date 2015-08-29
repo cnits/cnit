9090beb09d02b77e8d471bf422d92c845e93575c
phongnhung <appstoregugle2310@gmail.com>
2015-08-29 09:55:18 +0700

    Hash - update code

--- sample/Php/Encrypt/Hash.php
+++ sample/Php/Encrypt/Hash.php
@@ -9,6 +9,7 @@ class Hash {
     public $str;
 
     public function __construct($_str){
+        //Todo: return string value with length = 32
         $this -> str = $_str;
     }
 
@@ -17,16 +18,24 @@ class Hash {
     }
 
     public function _sha1(){
+        //Todo: return string value with length = 40
         return sha1($this -> str);
     }
 
     public function _crc32(){
+        //Todo: return value is integer type with 32 bits
         return crc32($this -> str);
     }
 
     public function _crypt(){
-        return crypt($this -> str);
+        //Todo: return string value with length = 13
+        return crypt($this -> str, "salt");
     }
+
 }
-$hash = new Hash("123");
-$hash -> _md5();
+$hash = new Hash("_hash");
+echo "<pre>";
+var_dump($hash -> _md5());
+var_dump($hash -> _sha1());
+var_dump($hash -> _crc32());
+var_dump($hash -> _crypt());
